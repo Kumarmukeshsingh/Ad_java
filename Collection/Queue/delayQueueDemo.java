428a60f25cde21e942c9adf62b62fec4247c38d7
mukesh singh <kumarmukeshsapna2001@gmail.com>
2025-10-16 00:07:22 +0530

    def of que

---
+++ Collection/Queue/delayQueueDemo.java
@@ -0,0 +1,10 @@
+public class delayQueueDemo {
+   public static void main(String[] args) {
+      // thread-safe unbounded blocking queue
+      // element can only be taken from the queue when their delay has expierd
+      // usefull for scheduling task to be executed after a certain dely
+      // internally priority queue
+      
+      
+   }
+}
